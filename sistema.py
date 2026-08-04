1b107cb5e1194976009ac980332ea02ec0ad068c
wembley M.S <wembleymacedo17@gmail.com>
2026-08-03 22:44:03 -0300

    Chave Rest Api

--- sistema.py
+++ sistema.py
@@ -3,8 +3,14 @@ import requests
 import time
 import cv2
 import numpy as np
+from load_dotenv import load_dotenv 
+import os
 
-API_BASE_URL = "http://127.0.0.1:8000/api/v1/retiradas"
+load_dotenv()  
+
+
+
+API_BASE_URL = os.getenv("API_BASE_URL")
 
 st.set_page_config(page_title="Entrega de Kits", page_icon="📦", layout="centered")
 
